3f0fdc5507d7d52532e6897ebe196a6ed2a9be24
Igor <igorko.s.v@gmail.com>
2016-08-25 14:57:16 +0300

    jquery json

--- myscript.js
+++ myscript.js
@@ -1,34 +1,74 @@
-var request;
-if (window.XMLHttpRequest) {
-	request=new XMLHttpRequest();
-} else {
-	request=new ActiveXObject("Microsoft.XMLHTTP");
-}
+// var request;
+// 	if (window.XMLHttpRequest) {
+// 		request=new XMLHttpRequest();
+// 	} else {
+// 		request=new ActiveXObject("Microsoft.XMLHTTP");
+// 	}
 
-request.open('GET', 'data.json');
-request.onreadystatechange = function() {
-	if ((request.status === 200) &&
-		(request.readyState === 4)) {
+// 	request.open('GET', 'https://grakha.github.io/data.json');
+// 	request.onreadystatechange = function() {
+// 		if ((request.status === 200) &&
+// 			(request.readyState === 4)) {
+
+// 				var info = JSON.parse(request.responseText);
+				
+// 				var output='';
+// 				for (var i = 0; i < info.links.length; i++) {
+// 					for (key in info.links[i]) {
+// 						if (info.links[i].hasOwnProperty(key)) {
+// 							output += '<li>' + 
+// 							'<a href = "' + info.links[i][key] +
+// 							'">' + key + '</a>';
+// 							'</li>';
+// 						}
+// 					}
+// 				}
+				
+// 				var update = document.getElementById('links');
+// 				update.innerHTML = output;
+				
+// 		}	//ready
+// 	}	//event
+
+// 	request.send();
+
+// function dataHandler(info){
+// 	var output='';
+// 	for (var i = 0; i < info.links.length; i++) {
+// 		for (key in info.links[i]) {
+// 			if (info.links[i].hasOwnProperty(key)) {
+// 				output += '<li>' + 
+// 				'<a href = "' + info.links[i][key] +
+// 				'">' + key + '</a>';
+// 				'</li>';
+// 			}
+// 		}
+// 	}
+
+// 	var update = document.getElementById('links');
+// 	update.innerHTML = output;
+// }
 
 
-			var info = JSON.parse(request.responseText);
-			
-			var output='';
-			for (var i = 0; i < info.links.length; i++) {
-				for (key in info.links[i]) {
-					if (info.links[i].hasOwnProperty(key)) {
-						output += '<li>' + 
-						'<a href = "' + info.links[i][key] +
-						'">' + key + '</a>';
-						'</li>';
-			    }   
+// For Jquery
+$(document).ready(function(){
+
+	$.getJSON('data.json', function(info) {
+
+		var output='';
+		for (var i = 0; i < info.links.length; i++) {
+			for (key in info.links[i]) {
+				if (info.links[i].hasOwnProperty(key)) {
+					output += '<li>' + 
+					'<a href = "' + info.links[i][key] +
+					'">' + key + '</a>';
+					'</li>';
 				}
 			}
-			
-			var update = document.getElementById('links');
-			update.innerHTML = output;
-			
-			
-	} //ready
-} //event
-request.send();
+		}
+		
+		var update = document.getElementById('links');
+		update.innerHTML = output;
+				
+	}); // get JSON
+}); // ready
